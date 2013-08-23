3e7d1fbaef52c36983817222cb0cf40cf2ed4044
Anders Pearson <anders@columbia.edu>
2013-08-23 09:48:20 -0400

    testing careerlocation models

--- pass_app/careerlocation/tests/test_models.py
+++ pass_app/careerlocation/tests/test_models.py
@@ -1,7 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from pass_app.careerlocation.models import MapLayer, ActorQuestion
+from pass_app.careerlocation.models import Actor, ActorResponse
+from pass_app.careerlocation.models import Strategy
 
 
-class BasicModelTest(TestCase):
+class MapLayerTest(TestCase):
+    def test_unicode(self):
+        m = MapLayer.objects.create(
+            name="test name", display_name="test display name")
+        self.assertEqual(str(m), "test display name")
 
-    def test_dummy(self):
-        assert True
+
+class ActorQuestionTest(TestCase):
+    def test_unicode(self):
+        aq = ActorQuestion.objects.create(question="short question")
+        self.assertEqual(str(aq), "short question")
+        aq.question = ''.join(str(x) for x in range(25))
+        self.assertEqual(str(aq), "0123456789101112131415161...")
+
+
+class ActorTest(TestCase):
+    def test_unicode(self):
+        a = Actor.objects.create(name="test actor", type="IV")
+        self.assertEqual(str(a), "Interview Stakeholders: test actor")
+
+
+class ActorResponseTest(TestCase):
+    def test_unicode(self):
+        u = User.objects.create(username="testuser")
+        a = Actor.objects.create(name="test actor", type="IV")
+        aq = ActorQuestion.objects.create(question="short question")
+        ar = ActorResponse.objects.create(actor=a, user=u, question=aq)
+        self.assertEqual(
+            str(ar),
+            "testuser [Interview Stakeholders: test actor : short question]"
+        )
+
+
+class StrategyTest(TestCase):
+    def test_unicode(self):
+        s = Strategy.objects.create(ordinal=1, title="test strategy")
+        self.assertEqual(str(s), "1. test strategy")
