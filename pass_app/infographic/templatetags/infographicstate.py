6af0d6a6206648bb6cb8e57035032bb350f346b2
Zarina Mustapha <zarina@columbia.edu>
2014-08-05 13:33:28 -0400

    fixed all the bugs for infographics

--- pass_app/infographic/templatetags/infographicstate.py
+++ pass_app/infographic/templatetags/infographicstate.py
@@ -6,13 +6,18 @@ register = template.Library()
 
 
 class GetUserState(template.Node):
+    def __init__(self, block):
+        self.block = block
+
     def render(self, context):
+        b = context[self.block]
         u = context['request'].user
 
         obj, create = InfographicState.objects.get_or_create(user=u)
-        return obj.items.values_list('id', flat=True)
+        return obj.items.filter(infographic=b).values_list('id', flat=True)
 
 
 @register.tag('get_user_state')
 def get_user_state(parser, token):
-    return GetUserState()
+    block = token.split_contents()[1:][0]
+    return GetUserState(block)
