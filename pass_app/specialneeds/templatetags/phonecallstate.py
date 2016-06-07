bc3de1511ea75dc1452052d25d5bbcca5c9617d2
Susan Dreher <sdreher@columbia.edu>
2016-06-07 13:22:17 -0400

    check for user

--- pass_app/specialneeds/templatetags/phonecallstate.py
+++ pass_app/specialneeds/templatetags/phonecallstate.py
@@ -1,5 +1,7 @@
 from django import template
-from pass_app.specialneeds.models import SpecialNeedsCallState
+
+from pass_app.specialneeds.models import \
+    SpecialNeedsCallState, SpecialNeedsCall
 
 
 register = template.Library()
@@ -7,6 +9,9 @@ register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def get_user_state(context, item):
+    if 'user' not in context:
+        return SpecialNeedsCall.objects.none()
+
     obj, create = SpecialNeedsCallState.objects.get_or_create(
         user=context['user'])
     return obj.questions.filter(id=item.id).count() > 0
